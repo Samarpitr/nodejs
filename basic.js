8dde2816c62643a9a0a88477895fd8f23101bbe0
Samarpit Arya <samarpit.cmunafa@gmail.com>
2017-04-05 15:55:07 +0530

    print, var, and const

---
+++ basic.js
@@ -0,0 +1,17 @@
+//To print something 
+console.log("Samarpit Arya")
+
+// Declare a variable
+var status = 1233
+console.log(status)
+//we can assign value to a variable
+status = 'sama'
+console.log(status)
+
+//making a constant 
+const status = 1234
+console.log(status)
+//Cannot assign value to a constant
+status = "samar"
+console.log(status)
+
